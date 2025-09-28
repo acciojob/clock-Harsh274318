81f2e064831c1eb88c99ae9f358e70d8e0c760ba
Harsh Vardhan Pal <hp77335@gmail.com>
2025-09-28 10:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+setInterval(()=>{
+        let time = document.getElementById("timer")
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let date1 = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear()
+    let timeZone = "AM"
+
+    if(hr>12){
+        hr = hr-12;
+        timeZone = "PM"
+    }
+    time.innerText= `${month}/${date1}/${year},${hr}:${min}:${sec} ${timeZone}`  
+    },1000)
